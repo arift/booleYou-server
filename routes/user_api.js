a4ea1b5557ae0fcaa6e6008452e08459120e2b3e
jtyler92 <jtyler@umass.edu>
2015-03-19 17:45:26 -0400

    Added booleOut schema and api

--- routes/user_api.js
+++ routes/user_api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var BooleOut = require('../models/booleOut');
 //returns ALL of the users
 router.route('/users').get(function(req, res) {
 	User.find(function(err, users) {
@@ -70,6 +71,54 @@ router.route('/users/:id').delete(function(req, res) {
  
     	res.json({ message: 'Successfully deleted' });
   	});
+});
+
+//returns ALL of the booleOuts
+router.route('/booleOuts').get(function(req, res) {
+  BooleOut.find(function(err, booleOuts) {
+    if (err) {
+      return res.send(err);
+      } 
+
+      res.json(booleOuts);
+    });
+});
+
+//adds a new booleOut
+router.route('/booleOuts').post(function(req, res) {
+    var booleOut = new BooleOut(req.body);
+ 
+    booleOut.save(function(err) {
+      if (err) {
+          return res.send(err);
+      }
+    console.log ("BooleOut \"" + booleOut.user_name + "\" added \"" + booleOut.bit + " " + booleOut.hashtag + "\"");
+      res.send({ message: 'booleOut Added' });
+    });
+});
+
+//returns a specific booleOut
+router.route('/booleOuts/:id').get(function(req, res) {
+    BooleOut.findOne({ _id: req.params.id}, function(err, booleOut) {
+      if (err) {
+          return res.send(err);
+      }
+ 
+      res.json(booleOut);
+    });
+});
+
+//deletes a specific booleOut
+router.route('/booleOuts/:id').delete(function(req, res) {
+  BooleOut.remove({
+      _id: req.params.id
+    }, function(err, movie) {
+      if (err) {
+          return res.send(err);
+      }
+ 
+      res.json({ message: 'Successfully deleted' });
+    });
 });
 
 module.exports = router;
