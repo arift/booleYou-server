b3d0d32383333d96f71aa7c93a7ac06cd3ba9bf2
Arif Topcu <09.arif@gmail.com>
2015-03-29 19:14:50 -0400

    booleout username

--- models/booleOut.js
+++ models/booleOut.js
@@ -4,7 +4,7 @@ var Schema=mongoose.Schema;
 var booleOutSchema = new Schema({
   	bit: Boolean,
   	hashtag: [String],
-  	user_name: String,
+  	userName: String,
   	post_date: {
     	type: Date,
     	default: Date.now
