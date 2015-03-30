a97d153304f7216c38d3f8e7f077d1dc8e490fab
Ryan Stanley <stanleyrya@gmail.com>
2015-03-30 16:26:37 -0400

    more work on test

--- test/test.js
+++ test/test.js
@@ -17,22 +17,37 @@ describe('firstTest', function () {
 
 });
 
-describe('GET /users', function(){
+describe('get all users', function(){
   it('respond with json', function (done){
-  	app.get('/users', function(req, res){
-  		res.status(200).send({ name: 'tobi' });
-	});
+  	// app.get('/users', function(req, res){
+  	// 	res.status(200).send({ name: 'tobi' });
+	  // });
 
     request(app)
       .get('/users')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       // .expect(200, done);
-      .expect(200)
+      // .expect(200)
       .end(function(err,res){
       	console.log(res,err);
       	if(err) return done(err);
       	done()
-      });
-  })
+    });
+  });
+});
+
+describe('account creation', function(){
+});
+
+describe('login', function(){
+});
+
+describe('look at user page', function(){
+});
+
+describe('post regular booleOut', function(){
+});
+
+describe('post reply booleOut', function(){
 });
