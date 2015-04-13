bb4293639d0acd883c0e7fb8e0b5924ca6f6c197
Arif <09.arif@gmail.com>
2015-04-13 12:20:42 -0400

    user bit related api calls

--- routes/api_user.js
+++ routes/api_user.js
@@ -40,9 +40,68 @@ router.route('/users/:username').get(function(req, res) {
   	User.findOne({ username: req.params.username}, function(err, user) {
     	if (err) {
       		return res.send(err);
-    	} 
+    	}
     	res.json(user);
   	});
+});
+
+// user receives 1
+router.route('/upbit/:username').get(function(req, res) {
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) {
+        return res.send(err);
+    }
+    user.bits += "1"; //add a 1 to the bits
+    user.ones += 1;  //increase the number of ones by one
+    user.save(function(err) {
+        if (err) {
+          return res.send(err);
+        }
+        console.log ("User, \"" + user.username + "\", received one upbit.");
+        res.json({ status: 'success', message: "User " + req.params.username + " recieved one upbit" });
+    });
+  });
+});
+
+// user receives 0
+router.route('/downbit/:username').get(function(req, res) {
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) {
+        return res.send(err);
+    }
+    user.bits += "0"; //add a 0 to the bits
+    user.zeros += 1;  //increase the number of zeros by one
+
+    // save the user
+    user.save(function(err) {
+        if (err) {
+          return res.send(err);
+        }
+        console.log ("User, \"" + user.username + "\", received one downbit.");
+        res.json({ status: 'success', msg: "User " + req.params.username + " recieved one downbit" });
+    });
+  });
+});
+
+//resets bits for a user
+router.route('/resetbits/:username').get(function(req, res) {
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) {
+        return res.send(err);
+    }
+    user.bits = "";
+    user.zeros = 0;
+    user.ones = 0;
+
+    // save the user
+    user.save(function(err) {
+        if (err) {
+          return res.send(err);
+        }
+        console.log ("User, \"" + user.username + "\", reset their bits.");
+        res.json({ status: 'success', msg: "User " + req.params.username + " reset bit information." });
+    });
+  });
 });
 
 //deletes a specific user
@@ -52,7 +111,7 @@ router.route('/users/:username').delete(function(req, res) {
   	}, function(err, data) {
     	if (err) {
       		return res.send(err);
-    	} 
+    	}
     	res.json({ message: 'Successfully deleted' });
   	});
 });
