629db96c1557e93bf4295344b7036701629ee433
Arif <09.arif@gmail.com>
2015-04-26 16:31:48 -0400

    All of the user APIs are now being tested

--- test/test.js
+++ test/test.js
@@ -85,7 +85,94 @@ describe('Routing', function(){
       .send(user)
       .expect(200, done);
     });
-
+    it('should create another user named mochatest2 (POST /auth/signup)', function(done){
+      var user = {
+        firstName : 'mochatest2',
+        lastName  : 'mochatest2',
+        email   : 'mochatest2@mochatest.com',
+        username  : "mochatest2",
+        password  : "mochatest2" 
+      };          
+      request(url)
+      .post('/auth/signup')
+      .send(user)
+      .expect(200, done);
+    });
+    it('should make user mochatest follow user mochatest2 (GET /api/user/mochatest/follow/mochatest2)', function(done){ 
+      request(url)
+      .get('/api/user/mochatest/follow/mochatest2')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        res.should.be.json;
+        var user = res.body.user;
+        user.should.be.ok;
+        user.firstName.should.equal('mochatest');
+        user.should.have.property('following').with.lengthOf(1);
+        user.following[0].should.equal('mochatest2');
+        done();
+      });
+    });
+    it('should request usermochatest2 and check to see if mochatest2 is being followed by mochatest (GET /api/user/users/mochatest2)', function(done){
+      request(url)
+      .get('/api/user/users/mochatest2')
+      .end(function(err, res) {
+        if (err) throw err;
+        res.should.be.json;
+        var user = res.body;
+        user.should.be.ok;
+        user.firstName.should.equal('mochatest2');
+        user.should.have.property('followedby').with.lengthOf(1);
+        user.followedby[0].should.equal('mochatest');
+        done();          
+      });
+    });
+    it('should make user mochatest unfollow user mochatest2 (GET /api/user/mochatest/unfollow/mochatest2)', function(done){ 
+      request(url)
+      .get('/api/user/mochatest/unfollow/mochatest2')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        res.should.be.json;
+        var user = res.body.user;
+        user.should.be.ok;
+        user.firstName.should.equal('mochatest');
+        user.should.have.property('following').with.lengthOf(0);
+        done();
+      });
+    });
+    it('should request usermochatest2 and check to see if the user mochatest is unfollowed (GET /api/user/users/mochatest2)', function(done){
+      request(url)
+      .get('/api/user/users/mochatest2')
+      .end(function(err, res) {
+        if (err) throw err;
+        res.should.be.json;
+        var user = res.body;
+        user.should.be.ok;
+        user.firstName.should.equal('mochatest2');
+        user.should.have.property('followedby').with.lengthOf(0);
+        done();          
+      });
+    });
+    it('should add a profile picture for user mochatest (POST /api/user/addpropic/mochatest', function(done){ 
+      var picture = {
+        username: 'mochatest',
+        picture  : "picturestring"
+      }; 
+      request(url)
+      .post('/api/user/addpropic/mochatest')
+      .send(picture)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        res.should.be.json;
+        var picture = res.body;
+        picture.should.be.ok;
+        picture.username.should.equal('mochatest');
+        picture.picture.should.equal('picturestring');
+        done();
+      });
+    });
   });
 
 	describe('booleOut related API calls', function(){
@@ -127,9 +214,19 @@ describe('Routing', function(){
         done();           
       });
     });   
-    it('should delete the user mochatest (GET /api/users/mochatest)', function(done){
+    it('should delete the user mochatest (DELETE /api/users/mochatest)', function(done){
       request(url)
       .delete('/api/user/users/mochatest')
+      .expect(200, done)
+    });
+    it('should delete the picture for the user mochatest (DELETE /api/deletepropic/mochatest)', function(done){
+      request(url)
+      .delete('/api/user/deletepropic/mochatest')
+      .expect(200, done)
+    });
+    it('should delete the user mochatest2 (DELETE /api/users/mochatest2)', function(done){
+      request(url)
+      .delete('/api/user/users/mochatest2')
       .expect(200, done)
     });
   });
