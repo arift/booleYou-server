4c6ebaded87633a0a8bc93e0856cc576b71e0953
Arif <09.arif@gmail.com>
2015-04-26 16:04:03 -0400

    added new tests

--- test/test.js
+++ test/test.js
@@ -11,41 +11,41 @@ describe('Routing', function(){
   		.get('/api/user/users')
   		.expect('Content-Type', /json/)
   		.expect(200)
-    		.end(function(err, res) {
-    			if(err) {
-    				throw err;
-    			}
-    			res.body.should.be.ok;
-    			done();
-    		});
+  		.end(function(err, res) {
+  			if(err) {
+  				throw err;
+  			}
+  			res.body.should.be.ok;
+  			done();
+  		});
   	});
   	it('should create a new user named mochatest (POST /auth/signup)', function(done){
-    		var user = {
-    			firstName	: 'mochatest',
-    			lastName	: 'mochatest',
-    			email		: 'mochatest@mochatest.com',
-    			username	: "mochatest",
-    			password	: "mochatest" 
-    		};      		
-    		request(url)
-    		.post('/auth/signup')
-    		.send(user)
-    		.expect(200, done);
+  		var user = {
+  			firstName	: 'mochatest',
+  			lastName	: 'mochatest',
+  			email		: 'mochatest@mochatest.com',
+  			username	: "mochatest",
+  			password	: "mochatest" 
+  		};      		
+  		request(url)
+  		.post('/auth/signup')
+  		.send(user)
+  		.expect(200, done);
   	});
   	it('should return the user mochatest (GET /api/users/mochatest)', function(done){
   		request(url)
   		.get('/api/user/users/mochatest')
-    		.end(function(err, res) {
-    			if(err) {
-    				throw err;
-    			}   
-    			res.body.should.be.ok;
-    			res.body.firstName.should.equal('mochatest');
-    			res.body.lastName.should.equal('mochatest');
-    			res.body.email.should.equal('mochatest@mochatest.com');
-    			res.body.username.should.equal('mochatest');	
-    			done();      			
-    		});
+  		.end(function(err, res) {
+  			if(err) {
+  				throw err;
+  			}   
+  			res.body.should.be.ok;
+  			res.body.firstName.should.equal('mochatest');
+  			res.body.lastName.should.equal('mochatest');
+  			res.body.email.should.equal('mochatest@mochatest.com');
+  			res.body.username.should.equal('mochatest');	
+  			done();      			
+  		});
   	});
   	it('should login the user mochatest (POST /auth/login)', function(done){
   		var user = {
@@ -57,6 +57,35 @@ describe('Routing', function(){
   		.send(user)
     	.expect(200, done);
   	});
+    it('should update the password of mochetest user (POST /api/user/changepass/mochatest)', function(done){
+      var data = {
+        newPassword  : "newpassword" 
+      };  
+      request(url)
+      .post('/api/user/changepass/mochatest')
+      .send(data)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        res.body.should.be.ok;
+        res.body.firstName.should.equal('mochatest');
+        res.body.lastName.should.equal('mochatest');
+        res.body.email.should.equal('mochatest@mochatest.com');
+        res.body.username.should.equal('mochatest');
+        done();
+      });
+    });
+    it('should login the user mochatest with the NEW password (POST /auth/login)', function(done){
+      var user = {
+        username  : "mochatest",
+        password  : "newpassword" 
+      };  
+      request(url)
+      .post('/auth/login')
+      .send(user)
+      .expect(200, done);
+    });
+
   });
 
 	describe('booleOut related API calls', function(){
@@ -83,18 +112,21 @@ describe('Routing', function(){
   			}
   			done();      			
   		});
-  	});
-  	it('should delete all the booleOuts posted by mochatest)', function(done){
-			request(url)
-  		.delete('/api/booleout/booleouts/mochatest')
-  		.expect(200)
-  		.end(function(err, res) {
-  			if(err) {
-  				throw err;
-  			}
-  			done();      			
-  		});
-  	});
+  	});  	
+  });
+
+  describe('Clean up calls', function(){ 
+    it('should delete all the booleOuts posted by mochatest)', function(done){
+      request(url)
+      .delete('/api/booleout/booleouts/mochatest')
+      .expect(200)
+      .end(function(err, res) {
+        if(err) {
+          throw err;
+        }
+        done();           
+      });
+    });   
     it('should delete the user mochatest (GET /api/users/mochatest)', function(done){
       request(url)
       .delete('/api/user/users/mochatest')
